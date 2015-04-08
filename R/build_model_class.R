4eabb7046bfd81bf7986b048b8be00414863648e
fernandosm <fernandosix@gmail.com>
2015-04-08 18:28:27 -0300

    deleting part of documentation

--- R/build_model_class.R
+++ R/build_model_class.R
@@ -11,17 +11,4 @@
 #' 
 #' @export
 #' @references .
-# @examples 
-# data(networkSample)
-# var.names <- list(from = 'originID', to = 'destinyID', Time = 'Dia',
-#                   arc = 'num.animais')
-# model.parms <- c(Beta = 1e-4)
-# init.cond <- c(I100525 = 10, I1155 = 10, I100324 = 10)
-#                   
-# model2simulate <- buildModelClass(structure(list(network = network.sample,
-#                                                  ssa.method = list(method = character(),
-#                                                                    tau = integer()),
-#                                                  pop.correc = TRUE,
-#                                                  nodes.info = nodes.information),
-#                                                  class = c('siWoDemogr', 'HM')))
 buildModelClass <- function(x, var.names, init.cond, model.parms) UseMethod("buildModelClass")

02e46a7398f330e2b75a662e2fe869d696ff062f
fernandosm <fernandosix@gmail.com>
2015-08-25 21:57:02 -0300

    adding plot options

--- R/plot_HM.R
+++ R/plot_HM.R
@@ -7,7 +7,15 @@
 #' 
 #' @param x \code{HM} object
 #' 
-#' @param sim which simulation to plot.
+#' @param sim points to which simulation to plot.
+#' 
+#' @param should scales be fixed ("free_y", the default), free ("free"), or free
+#'        in one dimension ("free_x", "free_y"). See ggplot2 package for more
+#'        details.
+#'        
+#' @param plot.type plots the mean number of each state variable for the whole
+#'        population ('pop.mean'), or the subpopulations of a particular
+#'        simulation ('subpop'), or the mean of each subpopulation ('subpop.mean').
 #' 
 #' @param ... arguments to be passed to methods
 #' 
@@ -31,31 +39,81 @@
 #' 
 #' plot(sim.results)
 
-plot.HM <- function(x, sim = 1, ...){
+plot.HM <- function(x, sim = 1, facet.scales = 'free_y', plot.type = 'subpop', ...){
   
   Time <- Number <- variable <- State <- NULL
   
-  sim.result <- x$results[which(x$results$sim == sim), ]
+  if(plot.type == 'subpop'){
+    sim.result <- x$results[which(x$results$sim == sim), ]
+    
+    sim.result.plot <- reshape2::melt(sim.result, id.vars = c('sim',
+                                                              x$ssaObjet$var.names$Time))
+    sim.result.plot[, 'State'] <- substring(sim.result.plot$variable, 1, 1)
+    colnames(sim.result.plot)[c(2,4)] <- c('Time','Number')
+    
+    sim.result.plot$State <- factor(sim.result.plot$State, levels = x$ssaObjet$state.var)
+    
+    ggplot2::ggplot(sim.result.plot, ggplot2::aes(x = Time, y = Number,
+                                                  group = variable, color = State)) + 
+      ggplot2::geom_line(alpha = 0.4, size = 0.3) + ggplot2::ggtitle(paste('Simulation', sim)) +
+      ggplot2::ylab('Number Of Individuals') + ggplot2::guides(color=FALSE) +
+      ggplot2::facet_wrap(~State, ncol = 1, scales = facet.scales) +      
+      ggplot2::theme(panel.border = ggplot2::element_rect(colour = "grey", fill = NA),
+                     strip.background = ggplot2::element_rect(fill = NA, colour = "grey", size = 0.1),
+                     strip.text = ggplot2::element_text(face = "bold", size = 7),
+                     panel.margin = grid::unit(0.6, "lines"),
+                     plot.title = ggplot2::element_text(size = 10, face = "bold"),
+                     axis.text = ggplot2::element_text(size = 6),
+                     axis.title = ggplot2::element_text(size = 8, face = "bold"))  
+  } else if(plot.type == 'pop.mean'){
+    sim.result <- x$results
+    sim.result.plot <- reshape2::melt(sim.result, id.vars = c('sim',
+                                                              x$ssaObjet$var.names$Time))
+    sim.result.plot[, 'State'] <- substring(sim.result.plot$variable, 1, 1)
+    sim.result.plot <- aggregate(sim.result.plot$value,
+                                 by = list(State = sim.result.plot$State,
+                                           Sim = sim.result.plot$sim,
+                                           Time = sim.result.plot[ , x$ssaObjet$var.names$Time]), sum)
+    sim.result.plot <- aggregate(sim.result.plot$x,
+                                 by = list(State = sim.result.plot$State,
+                                           Time = sim.result.plot$Time), mean)
+    sim.result.plot$State <- factor(sim.result.plot$State, levels = x$ssaObjet$state.var)
+    
+    ggplot2::ggplot(sim.result.plot, ggplot2::aes(x = Time, y = x,
+                                                  group = State, color = State)) + 
+      ggplot2::geom_line(size = 0.3) + ggplot2::ggtitle('Population') +
+      ggplot2::ylab('Mean Number Of Individuals') + ggplot2::guides(color=FALSE) +
+      ggplot2::facet_wrap(~State, ncol = 1, scales = facet.scales) +      
+      ggplot2::theme(panel.border = ggplot2::element_rect(colour = "grey", fill = NA),
+                     strip.background = ggplot2::element_rect(fill = NA, colour = "grey", size = 0.1),
+                     strip.text = ggplot2::element_text(face = "bold", size = 7),
+                     panel.margin = grid::unit(0.6, "lines"),
+                     plot.title = ggplot2::element_text(size = 10, face = "bold"),
+                     axis.text = ggplot2::element_text(size = 6),
+                     axis.title = ggplot2::element_text(size = 8, face = "bold"))
+  } else if(plot.type == 'subpop.mean'){
+    sim.result <- x$results
+    sim.result.plot <- reshape2::melt(sim.result, id.vars = c('sim',
+                                                              x$ssaObjet$var.names$Time))
+    sim.result.plot <- aggregate(sim.result.plot$value,
+                                 by = list(Subpop = sim.result.plot$variable,
+                                           Time = sim.result.plot[ , x$ssaObjet$var.names$Time]), mean)
+    
+    sim.result.plot[, 'State'] <- substring(sim.result.plot$Subpop, 1, 1)
+    sim.result.plot$State <- factor(sim.result.plot$State, levels = x$ssaObjet$state.var)
+    
+    ggplot2::ggplot(sim.result.plot, ggplot2::aes(x = Time, y = x,
+                                                  group = Subpop, color = State)) + 
+      ggplot2::geom_line(alpha = 0.4, size = 0.3) + ggplot2::ggtitle('Subpopulations') +
+      ggplot2::ylab('Mean Number Of Individuals') + ggplot2::guides(color=FALSE) +
+      ggplot2::facet_wrap(~State, ncol = 1, scales = facet.scales) +      
+      ggplot2::theme(panel.border = ggplot2::element_rect(colour = "grey", fill = NA),
+                     strip.background = ggplot2::element_rect(fill = NA, colour = "grey", size = 0.1),
+                     strip.text = ggplot2::element_text(face = "bold", size = 7),
+                     panel.margin = grid::unit(0.6, "lines"),
+                     plot.title = ggplot2::element_text(size = 10, face = "bold"),
+                     axis.text = ggplot2::element_text(size = 6),
+                     axis.title = ggplot2::element_text(size = 8, face = "bold"))
+  }
   
-  sim.result.plot <- reshape2::melt(sim.result, id.vars = c('sim',
-                                                            x$ssaObjet$var.names$Time))
-  sim.result.plot[, 'State'] <- substring(sim.result.plot$variable, 1, 1)
-  colnames(sim.result.plot)[c(2,4)] <- c('Time','Number')
-  
-  ggplot2::ggplot(sim.result.plot, ggplot2::aes(x = Time, y = Number,
-                                                group = variable, color = State)) + 
-    ggplot2::geom_line() + ggplot2::ggtitle(paste('Evolution of Simulation', sim)) +
-    ggplot2::ylab('Number Of Individuals') +
-    ggplot2::ylim(c(0, max(sim.result[,-which(colnames(sim.result) == x$ssaObjet$var.names$Time)])))
-
-  #   sim.result.plot.aggr <- aggregate(sim.result.plot$Number,
-  #                                     by = list(sim.result.plot$State, sim.result.plot$Time),
-  #                                     sum)
-  #   names(sim.result.plot.aggr) <- c('State', 'Time', 'Number')
-  #   
-  #   plot.aggr <- ggplot2::ggplot(sim.result.plot.aggr,
-  #                                ggplot2::aes(x = Time, y = Number, color = State)) +
-  #     ggplot2::geom_line() + ggplot2::ggtitle(paste('Evolution of Simulation', sim))
-  #   
-  #   print(plot.aggr)
-}
+}  

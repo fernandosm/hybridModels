d004adb1b951c8ae90ea25d3c1fe1373f8c378c4
fernandosm <fernandosix@gmail.com>
2016-01-13 17:21:59 -0200

    Example is now using custom equations and user can costumize ggplot layout

--- R/plot_HM.R
+++ R/plot_HM.R
@@ -9,39 +9,61 @@
 #' 
 #' @param sim points to which simulation to plot.
 #' 
-#' @param should scales be fixed ("free_y", the default), free ("free"), or free
+#' @param facet.scales should scales be fixed ("free_y", the default), free ("free"), or free
 #'        in one dimension ("free_x", "free_y"). See ggplot2 package for more
 #'        details.
 #'        
 #' @param plot.type plots the mean number of each state variable for the whole
 #'        population ('pop.mean'), or the subpopulations of a particular
 #'        simulation ('subpop'), or the mean of each subpopulation ('subpop.mean').
+#'        
+#' @param ... arguments to be passed to methods.
 #' 
-#' @param ... arguments to be passed to methods
+#' @export
 #' 
-#' @export plot.HM
 #' @examples 
 #' # Parameters and initial conditions.
-#' data(networkSample, nodesCensus)
-#' networkSample <- networkSample[which(networkSample$Dia < "2012-01-25"),]
+#' data(networkSample)
+#' networkSample <- networkSample[which(networkSample$Dia < "2012-06-30"),]
+#' 
 #' var.names <- list(from = 'originID', to = 'destinyID', Time = 'Dia',
 #'                   arc = 'num.animais')
-#' model.parms <- c(Beta = 1e-4)
-#' init.cond <- c(I100525 = 10, I1155 = 10, I100324 = 10)
+#'                   
+#' prop.func <- c('beta * S * I / (S + I + R)', 'gamma * I')
+#' state.var <- c('S', 'I', 'R')
+#' state.change.matrix <- matrix(c(-1,  0, # S
+#'                                  1, -1, # I
+#'                                  0,  1), # R
+#'                               nrow = 3, ncol = 2, byrow = TRUE)
+#'                               
+#' model.parms <- c(beta = 0.1, gamma = 0.01)
+#' 
+#' init.cond <- rep(200, length(unique(c(networkSample$originID,
+#'                                       networkSample$destinyID))))
+#' names(init.cond) <- paste('S', unique(c(networkSample$originID,
+#'                                         networkSample$destinyID)), sep = '')
+#' init.cond <- c(init.cond, c(I36811 = 50, I36812 = 50)) # adding infection
 #'                   
 #' # running simulations 
-#' sim.results <- hybridModel(network = networkSample, var.names,
-#'                            nodesCensus = nodesCensus,
-#'                            model.parms = model.parms,
-#'                            model = 'SI model without demographics',
-#'                            sim.number = 1, init.cond = init.cond,
-#'                            num.cores = 1)
+#' sim.results <- hybridModel(network = networkSample, var.names = var.names,
+#'                            model.parms = model.parms, state.var = state.var,
+#'                            prop.func = prop.func, init.cond = init.cond,
+#'                            state.change.matrix = state.change.matrix,
+#'                            sim.number = 2, num.cores = 'max')
 #' 
-#' plot(sim.results)
-
-plot.HM <- function(x, sim = 1, facet.scales = 'free_y', plot.type = 'subpop', ...){
+#' # default plot layout (plot.types: 'pop.mean', 'subpop', or 'subpop.mean')
+#' plot(sim.results, plot.type = 'subpop.mean')
+#' 
+#' # changing plot layout with ggplot2 (example)
+#' library(ggplot2)
+#' plot(sim.results, plot.type = 'subpop') + ggtitle('New Layout') + 
+#'    theme(axis.title = element_text(size = 14, face = "italic"))
+#'    
+plot.HM <- function(x, sim = 1, plot.type = 'subpop', facet.scales = 'free_y', ...){
   
-  Time <- Number <- variable <- State <- NULL
+  plot.type <- match.arg(plot.type, c('subpop', 'pop.mean', 'subpop.mean'))
+  
+  Time <- Number <- variable <- State <- Subpop <- NULL
   
   if(plot.type == 'subpop'){
     sim.result <- x$results[which(x$results$sim == sim), ]
@@ -53,18 +75,18 @@ plot.HM <- function(x, sim = 1, facet.scales = 'free_y', plot.type = 'subpop', .
     
     sim.result.plot$State <- factor(sim.result.plot$State, levels = x$ssaObjet$state.var)
     
-    ggplot2::ggplot(sim.result.plot, ggplot2::aes(x = Time, y = Number,
-                                                  group = variable, color = State)) + 
-      ggplot2::geom_line(alpha = 0.4, size = 0.3) + ggplot2::ggtitle(paste('Simulation', sim)) +
-      ggplot2::ylab('Number Of Individuals') + ggplot2::guides(color=FALSE) +
-      ggplot2::facet_wrap(~State, ncol = 1, scales = facet.scales) +      
-      ggplot2::theme(panel.border = ggplot2::element_rect(colour = "grey", fill = NA),
-                     strip.background = ggplot2::element_rect(fill = NA, colour = "grey", size = 0.1),
-                     strip.text = ggplot2::element_text(face = "bold", size = 7),
-                     panel.margin = grid::unit(0.6, "lines"),
-                     plot.title = ggplot2::element_text(size = 10, face = "bold"),
-                     axis.text = ggplot2::element_text(size = 6),
-                     axis.title = ggplot2::element_text(size = 8, face = "bold"))  
+    return(ggplot2::ggplot(sim.result.plot, ggplot2::aes(x = Time, y = Number,
+                                                         group = variable, color = State)) + 
+             ggplot2::geom_line(alpha = 0.4, size = 0.3) + ggplot2::ggtitle(paste('Simulation', sim)) +
+             ggplot2::ylab('Number Of Individuals') + ggplot2::guides(color=FALSE) +
+             ggplot2::facet_wrap(~State, ncol = 1, scales = facet.scales) +      
+             ggplot2::theme(panel.border = ggplot2::element_rect(colour = "grey", fill = NA),
+                            strip.background = ggplot2::element_rect(fill = NA, colour = "grey", size = 0.1),
+                            strip.text = ggplot2::element_text(face = "bold", size = 12),
+                            panel.margin = grid::unit(0.6, "lines"),
+                            plot.title = ggplot2::element_text(size = 14, face = "bold"),
+                            axis.text = ggplot2::element_text(size = 10),
+                            axis.title = ggplot2::element_text(size = 12, face = "bold")))
   } else if(plot.type == 'pop.mean'){
     sim.result <- x$results
     sim.result.plot <- reshape2::melt(sim.result, id.vars = c('sim',
@@ -79,18 +101,18 @@ plot.HM <- function(x, sim = 1, facet.scales = 'free_y', plot.type = 'subpop', .
                                            Time = sim.result.plot$Time), mean)
     sim.result.plot$State <- factor(sim.result.plot$State, levels = x$ssaObjet$state.var)
     
-    ggplot2::ggplot(sim.result.plot, ggplot2::aes(x = Time, y = x,
-                                                  group = State, color = State)) + 
-      ggplot2::geom_line(size = 0.3) + ggplot2::ggtitle('Population') +
-      ggplot2::ylab('Mean Number Of Individuals') + ggplot2::guides(color=FALSE) +
-      ggplot2::facet_wrap(~State, ncol = 1, scales = facet.scales) +      
-      ggplot2::theme(panel.border = ggplot2::element_rect(colour = "grey", fill = NA),
-                     strip.background = ggplot2::element_rect(fill = NA, colour = "grey", size = 0.1),
-                     strip.text = ggplot2::element_text(face = "bold", size = 7),
-                     panel.margin = grid::unit(0.6, "lines"),
-                     plot.title = ggplot2::element_text(size = 10, face = "bold"),
-                     axis.text = ggplot2::element_text(size = 6),
-                     axis.title = ggplot2::element_text(size = 8, face = "bold"))
+    return(ggplot2::ggplot(sim.result.plot, ggplot2::aes(x = Time, y = x,
+                                                         group = State, color = State)) + 
+             ggplot2::geom_line(size = 0.3) + ggplot2::ggtitle('Population') +
+             ggplot2::ylab('Mean Number Of Individuals') + ggplot2::guides(color=FALSE) +
+             ggplot2::facet_wrap(~State, ncol = 1, scales = facet.scales) +      
+             ggplot2::theme(panel.border = ggplot2::element_rect(colour = "grey", fill = NA),
+                            strip.background = ggplot2::element_rect(fill = NA, colour = "grey", size = 0.1),
+                            strip.text = ggplot2::element_text(face = "bold", size = 12),
+                            panel.margin = grid::unit(0.6, "lines"),
+                            plot.title = ggplot2::element_text(size = 14, face = "bold"),
+                            axis.text = ggplot2::element_text(size = 10),
+                            axis.title = ggplot2::element_text(size = 12, face = "bold")))
   } else if(plot.type == 'subpop.mean'){
     sim.result <- x$results
     sim.result.plot <- reshape2::melt(sim.result, id.vars = c('sim',
@@ -102,18 +124,17 @@ plot.HM <- function(x, sim = 1, facet.scales = 'free_y', plot.type = 'subpop', .
     sim.result.plot[, 'State'] <- substring(sim.result.plot$Subpop, 1, 1)
     sim.result.plot$State <- factor(sim.result.plot$State, levels = x$ssaObjet$state.var)
     
-    ggplot2::ggplot(sim.result.plot, ggplot2::aes(x = Time, y = x,
-                                                  group = Subpop, color = State)) + 
-      ggplot2::geom_line(alpha = 0.4, size = 0.3) + ggplot2::ggtitle('Subpopulations') +
-      ggplot2::ylab('Mean Number Of Individuals') + ggplot2::guides(color=FALSE) +
-      ggplot2::facet_wrap(~State, ncol = 1, scales = facet.scales) +      
-      ggplot2::theme(panel.border = ggplot2::element_rect(colour = "grey", fill = NA),
-                     strip.background = ggplot2::element_rect(fill = NA, colour = "grey", size = 0.1),
-                     strip.text = ggplot2::element_text(face = "bold", size = 7),
-                     panel.margin = grid::unit(0.6, "lines"),
-                     plot.title = ggplot2::element_text(size = 10, face = "bold"),
-                     axis.text = ggplot2::element_text(size = 6),
-                     axis.title = ggplot2::element_text(size = 8, face = "bold"))
+    return(ggplot2::ggplot(sim.result.plot, ggplot2::aes(x = Time, y = x,
+                                                         group = Subpop, color = State)) + 
+             ggplot2::geom_line(alpha = 0.4, size = 0.3) + ggplot2::ggtitle('Subpopulations') +
+             ggplot2::ylab('Mean Number Of Individuals') + ggplot2::guides(color=FALSE) +
+             ggplot2::facet_wrap(~State, ncol = 1, scales = facet.scales) +      
+             ggplot2::theme(panel.border = ggplot2::element_rect(colour = "grey", fill = NA),
+                            strip.background = ggplot2::element_rect(fill = NA, colour = "grey", size = 0.1),
+                            strip.text = ggplot2::element_text(face = "bold", size = 12),
+                            panel.margin = grid::unit(0.6, "lines"),
+                            plot.title = ggplot2::element_text(size = 14, face = "bold"),
+                            axis.text = ggplot2::element_text(size = 10),
+                            axis.title = ggplot2::element_text(size = 12, face = "bold")))
   }
-  
-}  
+}

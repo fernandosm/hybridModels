e6f95377e12b1f153c360516b494709eb46ff3ad
fernandosm <fernandosix@gmail.com>
2016-01-14 18:43:42 -0200

    back to doSNOW package

--- R/sim_SI_wo_demogr_infl.R
+++ R/sim_SI_wo_demogr_infl.R
@@ -149,7 +149,7 @@ simHM.siWoDemogrInfl <- function(x, network, sim.number, num.cores){
     num.cores <- parallel::detectCores() 
   }
   cl <- parallel::makeCluster(num.cores, type = "SOCK")
-  doParallel::registerDoParallel(cl)
+  doSNOW::registerDoSNOW(cl)
   sims <- NULL
   sim.result <- foreach(sims = 1:sim.number, .verbose=FALSE, .inorder=FALSE,
                         .packages = 'GillespieSSA') %dopar% (siWoDemogrInfl())
--- R/sim_SI_wo_demogr_migr.R
+++ R/sim_SI_wo_demogr_migr.R
@@ -184,7 +184,7 @@ simHM.siWoDemogrMigr <- function(x, network, sim.number, num.cores){
     num.cores <- parallel::detectCores() 
   }
   cl <- parallel::makeCluster(num.cores, type = "SOCK")
-  doParallel::registerDoParallel(cl)
+  doSNOW::registerDoSNOW(cl)
   sims <- NULL
   sim.result <- foreach(sims = 1:sim.number, .verbose=FALSE, .inorder=FALSE,
                         .packages = 'GillespieSSA') %dopar% (siWoDemogrMigr())
